70e872269712407b8b4229eae1fd61a320f2d798
SabinaC4008 <schauhan7@horizon.csueastbay.edu>
2024-02-27 23:44:59 -0800

    Update formJavascript.js

--- Code/Project 1/Project 1/Project Pages/JavaScript/formJavascript.js
+++ Code/Project 1/Project 1/Project Pages/JavaScript/formJavascript.js
@@ -1,11 +1,11 @@
 // JavaScript Document
-LoginEmail(emailInput) {
-     return (emailInput.length < 8) return false;
-     if (item.indexOf ('@') == -1) return false;
+function LoginEmail(emailInput) {
+     if (emailInput.length < 8) return false;
+     if (emailInput.indexOf ('@') == -1) return false;
      return true;
 }
 function LoginUsername(usernameInput) {
-	return (username.length < 8);
+	return (usernameInput.length < 8);
 }
 
 function LoginPasswords(passwordInput) {
@@ -20,15 +20,15 @@ function ValidForm(){
 	var pass = document.createaccountform.password_input.value;
 	var useremail = document.createaccountform.email_input.value;
 	if (!LoginUsername(user)){
-		alert("Please make sure your username is at least 8 characters long.");
+		window.alert("Please make sure your username is at least 8 characters long.");
 		return false;
 	}
 	if (!LoginPasswords(pass)){
-		alert("Please make sure your password has at least 10 characters with at least 1 number and one letter.");
+		window.alert("Please make sure your password has at least 10 characters with at least 1 number and one letter.");
 		return false;
 	}
 	if (!LoginEmail(useremail)){
-		alert("Invalid Email!");
+		window.alert("Invalid Email!");
 		return false;
 	}
 	return true;
